67fb56922e4a105a1c34b5ce12bcfc295b5de5fb
Shramee Srivastav <shramee.srivastav@gmail.com>
2025-08-09 13:28:51 +0300

    chore: shared circuit points

--- code/sw_test.go
+++ code/sw_test.go
@@ -10,19 +10,16 @@ import (
 	sw_towered "sota_pairing/sw_bn254_towered"
 )
 
-type Bench3_Pair_PairingsInR1CS struct {
-	In1G1 sw_towered.G1Affine
-	In2G1 sw_towered.G1Affine
-	In1G2 sw_towered.G2Affine
-	In2G2 sw_towered.G2Affine
+type Bench3_Pair_PiR1 struct {
+	Pairs TwoPairs[sw_towered.G1Affine, sw_towered.G2Affine]
 }
 
-func (c *Bench3_Pair_PairingsInR1CS) Define(api frontend.API) error {
+func (c *Bench3_Pair_PiR1) Define(api frontend.API) error {
 	pairing, err := sw_towered.NewPairing(api)
 	if err != nil {
 		return fmt.Errorf("new pairing: %w", err)
 	}
-	el, err := pairing.Pair([]*sw_towered.G1Affine{&c.In1G1, &c.In2G1}, []*sw_towered.G2Affine{&c.In1G2, &c.In2G2})
+	el, err := pairing.Pair([]*sw_towered.G1Affine{&c.Pairs.In1G1, &c.Pairs.In2G1}, []*sw_towered.G2Affine{&c.Pairs.In1G2, &c.Pairs.In2G2})
 	if err != nil {
 		return fmt.Errorf("pair: %w", err)
 	}
@@ -30,29 +27,22 @@ func (c *Bench3_Pair_PairingsInR1CS) Define(api frontend.API) error {
 	return nil
 }
 
-func (c *Bench3_Pair_PairingsInR1CS) Init() Benchmarkable {
-	p1, p2, q1, q2 := RandomG1G2AffinesPair()
-	return &Bench3_Pair_PairingsInR1CS{
-		In1G1: sw_towered.NewG1Affine(p1),
-		In1G2: sw_towered.NewG2Affine(q1),
-		In2G1: sw_towered.NewG1Affine(p2),
-		In2G2: sw_towered.NewG2Affine(q2),
+func (c *Bench3_Pair_PiR1) Init() Benchmarkable {
+	return &Bench3_Pair_PiR1{
+		Pairs: RandomToweredPairs(),
 	}
 }
 
-type Bench4_Pair_FasterFieldExtensionMuls struct {
-	In1G1 G1Affine
-	In2G1 G1Affine
-	In1G2 G2Affine
-	In2G2 G2Affine
+type Bench4_Pair_FXFM struct {
+	Pairs TwoPairs[G1Affine, G2Affine]
 }
 
-func (c *Bench4_Pair_FasterFieldExtensionMuls) Define(api frontend.API) error {
+func (c *Bench4_Pair_FXFM) Define(api frontend.API) error {
 	pairing, err := NewPairing(api)
 	if err != nil {
 		return fmt.Errorf("new pairing: %w", err)
 	}
-	el, err := pairing.Pair([]*G1Affine{&c.In1G1, &c.In2G1}, []*G2Affine{&c.In1G2, &c.In2G2})
+	el, err := pairing.Pair([]*G1Affine{&c.Pairs.In1G1, &c.Pairs.In2G1}, []*G2Affine{&c.Pairs.In1G2, &c.Pairs.In2G2})
 	if err != nil {
 		return fmt.Errorf("pair: %w", err)
 	}
@@ -60,48 +50,37 @@ func (c *Bench4_Pair_FasterFieldExtensionMuls) Define(api frontend.API) error {
 	return nil
 }
 
-func (c *Bench4_Pair_FasterFieldExtensionMuls) Init() Benchmarkable {
-	p1, p2, q1, q2 := RandomG1G2AffinesPair()
-	return &Bench4_Pair_FasterFieldExtensionMuls{
-		In1G1: NewG1Affine(p1),
-		In1G2: NewG2Affine(q1),
-		In2G1: NewG1Affine(p2),
-		In2G2: NewG2Affine(q2),
+func (c *Bench4_Pair_FXFM) Init() Benchmarkable {
+	return &Bench4_Pair_FXFM{
+		Pairs: RandomPairs(),
 	}
 }
 
-type Bench5_Pair_EliminatingFinalExponentiation struct {
-	In1G1 G1Affine
-	In2G1 G1Affine
-	In1G2 G2Affine
-	In2G2 G2Affine
+type Bench5_Pair_ElFX struct {
+	Pairs TwoPairs[G1Affine, G2Affine]
 }
 
-func (c *Bench5_Pair_EliminatingFinalExponentiation) Define(api frontend.API) error {
+func (c *Bench5_Pair_ElFX) Define(api frontend.API) error {
 	pairing, err := NewPairing(api)
 	if err != nil {
 		return fmt.Errorf("new pairing: %w", err)
 	}
-	err = pairing.PairingCheck([]*G1Affine{&c.In1G1, &c.In2G1}, []*G2Affine{&c.In1G2, &c.In2G2})
+	err = pairing.PairingCheck([]*G1Affine{&c.Pairs.In1G1, &c.Pairs.In2G1}, []*G2Affine{&c.Pairs.In1G2, &c.Pairs.In2G2})
 	if err != nil {
 		return fmt.Errorf("pair: %w", err)
 	}
 	return nil
 }
 
-func (c *Bench5_Pair_EliminatingFinalExponentiation) Init() Benchmarkable {
-	p1, p2, q1, q2 := RandomG1G2AffinesPair()
-	return &Bench5_Pair_EliminatingFinalExponentiation{
-		In1G1: NewG1Affine(p1),
-		In1G2: NewG2Affine(q1),
-		In2G1: NewG1Affine(p2),
-		In2G2: NewG2Affine(q2),
+func (c *Bench5_Pair_ElFX) Init() Benchmarkable {
+	return &Bench5_Pair_ElFX{
+		Pairs: RandomPairs(),
 	}
 }
 
 // bench
 func BenchmarkPairing(b *testing.B) {
-	BenchmarkCircuit(&Bench3_Pair_PairingsInR1CS{}, b)
-	BenchmarkCircuit(&Bench4_Pair_FasterFieldExtensionMuls{}, b)
-	BenchmarkCircuit(&Bench5_Pair_EliminatingFinalExponentiation{}, b)
+	BenchmarkCircuit(&Bench3_Pair_PiR1{}, b)
+	BenchmarkCircuit(&Bench4_Pair_FXFM{}, b)
+	BenchmarkCircuit(&Bench5_Pair_ElFX{}, b)
 }
--- code/test_utils.go
+++ code/test_utils.go
@@ -3,8 +3,19 @@ package sota_pairing
 import (
 	"crypto/rand"
 
+	sw_towered "sota_pairing/sw_bn254_towered"
+
+	. "sota_pairing/sw_bn254"
+
 	"github.com/consensys/gnark-crypto/ecc/bn254"
 )
+
+type TwoPairs[T1, T2 any] struct {
+	In1G1 T1
+	In2G1 T1
+	In1G2 T2
+	In2G2 T2
+}
 
 func RandomG1G2Affines() (p bn254.G1Affine, q bn254.G2Affine, err error) {
 	_, _, G1AffGen, G2AffGen := bn254.Generators()
@@ -31,4 +42,24 @@ func RandomG1G2AffinesPair() (p1, p2 bn254.G1Affine, q1, q2 bn254.G2Affine) {
 	q2.Set(&q1)
 	q1.Double(&q1)
 	return
+}
+
+func RandomToweredPairs() TwoPairs[sw_towered.G1Affine, sw_towered.G2Affine] {
+	p1, p2, q1, q2 := RandomG1G2AffinesPair()
+	return TwoPairs[sw_towered.G1Affine, sw_towered.G2Affine]{
+		In1G1: sw_towered.NewG1Affine(p1),
+		In2G1: sw_towered.NewG1Affine(p2),
+		In1G2: sw_towered.NewG2Affine(q1),
+		In2G2: sw_towered.NewG2Affine(q2),
+	}
+}
+
+func RandomPairs() TwoPairs[G1Affine, G2Affine] {
+	p1, p2, q1, q2 := RandomG1G2AffinesPair()
+	return TwoPairs[G1Affine, G2Affine]{
+		In1G1: NewG1Affine(p1),
+		In2G1: NewG1Affine(p2),
+		In1G2: NewG2Affine(q1),
+		In2G2: NewG2Affine(q2),
+	}
 }
