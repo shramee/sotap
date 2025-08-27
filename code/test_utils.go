ee10386a21127975ff17c632c670c0a549f69069
Shramee Srivastav <shramee.srivastav@gmail.com>
2025-08-28 00:21:56 +0300

    add e12 test utils

--- code/test_utils.go
+++ code/test_utils.go
@@ -3,6 +3,8 @@ package sota_pairing
 import (
 	"crypto/rand"
 
+	fields "sota_pairing/fields_bn254"
+	fields_towered "sota_pairing/fields_bn254_towered"
 	sw_towered "sota_pairing/sw_bn254_towered"
 
 	. "sota_pairing/sw_bn254"
@@ -15,6 +17,10 @@ type TwoPairs[T1, T2 any] struct {
 	In2G1 T1
 	In1G2 T2
 	In2G2 T2
+}
+
+type MulEls[T1 any] struct {
+	A, B, C T1
 }
 
 func RandomG1G2Affines() (p bn254.G1Affine, q bn254.G2Affine, err error) {
@@ -44,6 +50,13 @@ func RandomG1G2AffinesPair() (p1, p2 bn254.G1Affine, q1, q2 bn254.G2Affine) {
 	return
 }
 
+func RandomE12Els() (a, b, c bn254.E12) {
+	a.SetRandom()
+	b.SetRandom()
+	c.Mul(&a, &b)
+	return
+}
+
 func RandomToweredPairs() TwoPairs[sw_towered.G1Affine, sw_towered.G2Affine] {
 	p1, p2, q1, q2 := RandomG1G2AffinesPair()
 	return TwoPairs[sw_towered.G1Affine, sw_towered.G2Affine]{
@@ -61,5 +74,23 @@ func RandomPairs() TwoPairs[G1Affine, G2Affine] {
 		In2G1: NewG1Affine(p2),
 		In1G2: NewG2Affine(q1),
 		In2G2: NewG2Affine(q2),
+	}
+}
+
+func RandomToweredE12Mul() MulEls[fields_towered.E12] {
+	a, b, c := RandomE12Els()
+	return MulEls[fields_towered.E12]{
+		A: fields_towered.FromE12(&a),
+		B: fields_towered.FromE12(&b),
+		C: fields_towered.FromE12(&c),
+	}
+}
+
+func RandomE12Mul() MulEls[GTEl] {
+	a, b, c := RandomE12Els()
+	return MulEls[GTEl]{
+		A: fields.FromE12(&a),
+		B: fields.FromE12(&b),
+		C: fields.FromE12(&c),
 	}
 }
